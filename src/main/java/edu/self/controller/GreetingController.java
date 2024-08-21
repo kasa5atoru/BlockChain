8168e30e949ecc81d3cd3448eec5c0477cb07369
kasa5atoru <kasa5atoru@gmail.com>
2024-08-21 18:19:45 +0900

    Swagger OpenAPI worked

--- src/main/java/edu/self/controller/GreetingController.java
+++ src/main/java/edu/self/controller/GreetingController.java
@@ -7,15 +7,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.self.entity.Greeting;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	@GetMapping("/")
+	public String home() {
+		log.debug("method home called");
+		return "Hello World";
+	}
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+	
+
 }
